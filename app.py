cbc34e922ec26240ac4daccfe7312b69a6a91134
Ayoub Nassiri <121341933+iyoub00@users.noreply.github.com>
2025-11-03 12:13:18 +0100

    ver 5 commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello user from ECR-deployed container!, version 4\n"
+    return "Hello user from ECR-deployed container!, version with updated task def\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
